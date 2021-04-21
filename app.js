b786b2447ac4c6e5e1ad3ec701a9dae7790dd684
Goodness <ojeaga47@gmail.com>
2021-04-21 15:30:41 +0100

    Users can see display of pictures

--- app.js
+++ app.js
@@ -16,8 +16,26 @@ const port = process.env.PORT || 4000
 
 
 app.get('/', (req, res)=>{
-    res.render('home', {message: 'Welcome peeps to handlebars!'})
+    res.render('events-index', { events: events })
 })
+
+
+
+
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+  ]
+  
+  // INDEX
+  app.get('/events', (req, res) => {
+    res.render('events-index', { events: events });
+  })
+
+
+
+
 
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`)
